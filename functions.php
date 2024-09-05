e2ef358d23ed7db188859a105cd6274cdddded4b
chiDraEVE <chidraeve@clubitsolutions.de>
2024-09-05 14:13:20 +0200

    weitere Schrift geladen und alter Style des Seitentitels übernommen

--- functions.php
+++ functions.php
@@ -3,6 +3,9 @@ function child_theme_styles() {
     // Libre-Franklin-Schrift laden
     wp_enqueue_style('libre-franklin', get_template_directory_uri() . '/libre-franklin.css');
 
+    // Old-Standard-TT-Schrift laden
+    wp_enqueue_style('old-standard-tt', get_template_directory_uri() . '/old-standard.css');
+
     // Laden des Parent-Theme Styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
