8d8d2ee49bc13226842986ed4f0b3b6b4b9e0b84
chiDraEVE <chidraeve@clubitsolutions.de>
2024-09-05 12:10:12 +0200

    wp scripts eingerichtet und erstes styling erstellt. Alte CSS integriert und angefangen mit der Navigation die individuellen Anpassungen ins Stylesheet zu übernehmen

---
+++ build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array(), 'version' => '11a9f3992934942d6326');
--- functions.php
+++ functions.php
@@ -7,7 +7,7 @@ function child_theme_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
     // Laden des Child-Theme Styles
-    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array( 'parent-style' ) );
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/build/style-index.css', array( 'parent-style' ) );
 }
 
 add_action( 'wp_enqueue_scripts', 'child_theme_styles' );
