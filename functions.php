574b16667875020d04e54cd000b48cb41892ab7e
chiDraEVE <chidraeve@clubitsolutions.de>
2024-09-09 16:34:37 +0200

    webpack erzeugt zusätzliches Bundle, dass noch mit in die functions.php aufgenommen werden muss

--- functions.php
+++ functions.php
@@ -1,13 +1,7 @@
 <?php
 function child_theme_styles() {
-//    // Libre-Franklin-Schrift laden
-//    wp_enqueue_style('libre-franklin', get_template_directory_uri() . '/libre-franklin.css');
-//
-//    // Old-Standard-TT-Schrift laden
-//    wp_enqueue_style('old-standard-tt', get_template_directory_uri() . '/old-standard.css');
-//
-//    // Old-Standard-TT-Schrift laden
-//    wp_enqueue_style('gabriela', get_template_directory_uri() . '/gabriela.css');
+    // Schriften und weitere Stylesheets laden
+    wp_enqueue_style('additional-styles', get_stylesheet_directory_uri() . '/build/index.css');
 
     // Laden des Parent-Theme Styles
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
